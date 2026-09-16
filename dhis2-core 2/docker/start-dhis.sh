d025b797f7fb284a5cc6a03849ef87c5c7d50c40
Jaymal Kalidas <Jaymal.Kalidas@quantium.co.za>
2026-09-16 17:28:30 +0200

    config updates

---
+++ dhis2-core 2/docker/start-dhis.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -eu
+
+: "${DB_HOSTNAME:?DB_HOSTNAME must be set}"
+: "${DB_PORT:=5432}"
+: "${DB_NAME:?DB_NAME must be set}"
+: "${DB_USERNAME:?DB_USERNAME must be set}"
+: "${DB_PASSWORD:?DB_PASSWORD must be set}"
+
+config="/tmp/dhis.conf"
+{
+  printf '%s\n' \
+    'connection.dialect = org.hibernate.dialect.PostgreSQLDialect' \
+    'connection.driver_class = org.postgresql.Driver'
+  printf 'connection.url = jdbc:postgresql://%s:%s/%s\n' \
+    "$DB_HOSTNAME" "$DB_PORT" "$DB_NAME"
+  printf 'connection.username = %s\n' "$DB_USERNAME"
+  printf 'connection.password = %s\n' "$DB_PASSWORD"
+  printf '%s\n' \
+    '' \
+    'tracker.import.preheat.cache.enabled=off' \
+    '' \
+    'server.https = off'
+} > "$config"
+
+cp "$config" /opt/dhis2/dhis.conf
+exec "$@"
